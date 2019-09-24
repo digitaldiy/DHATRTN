6eb148767a054a95d77d05f77c16bb8590007fdd
digitaldiy <36301244+digitaldiy@users.noreply.github.com>
2019-09-24 20:01:42 +0100

    relay test

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -4,29 +4,18 @@ import { graphql } from 'gatsby'
 import PageHeader from '../components/PageHeader'
 import Content from '../components/Content'
 import Layout from '../components/Layout'
+import Relay from '../components/Relay/relay'
 
 // Export Template for use in CMS preview
 export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
-  <main className="Home">
-    <PageHeader
-      large
-      title={title}
-      subtitle={subtitle}
-      backgroundImage={featuredImage}
-    />
-
-    <section className="section">
-      <div className="container">
-        <Content source={body} />
-      </div>
-    </section>
-  </main>
+  <Relay/>
 )
 
 // Export Default HomePage for front-end
 const HomePage = ({ data: { page } }) => (
   <Layout meta={page.frontmatter.meta || false}>
     <HomePageTemplate {...page} {...page.frontmatter} body={page.html} />
+ 
   </Layout>
 )
 
